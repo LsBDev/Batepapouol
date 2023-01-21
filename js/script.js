8cc52ba8b55128f4a7a113ad33145754cc550833
profluifel <prof.luifel@gmail.com>
2023-01-21 20:31:46 -0300

    Quase finalizado (ajustes)

--- js/script.js
+++ js/script.js
@@ -2,12 +2,9 @@ const nomeUsuario = prompt("Qual seu nome?");
 let usuario = {name: nomeUsuario};
 let mensagens;
 let lastMessage;
-
-
 let resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
 
 resposta.then(logado, naoLogado);
-
 function logado() {
   return;
 }
@@ -19,9 +16,7 @@ function naoLogado(off) {
   }
 }
 
-// console.log(usuario);
-
-setInterval(estaLogado, 4000);
+setInterval(estaLogado, 5000);
 function estaLogado() {
   axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuario).then(
   () => {
@@ -32,14 +27,12 @@ function estaLogado() {
   }
   );
 }
-
 let promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
 promise.then(deuCerto, deuErro);
-
 function deuCerto(sucesso) {
   mensagens = sucesso.data;
   mostrarUltima();
-  setInterval(()=>{axios.get('https://mock-api.driven.com.br/api/v6/uol/messages').then(mostrarMensagens)}, 1000);
+  setInterval(()=>{axios.get('https://mock-api.driven.com.br/api/v6/uol/messages').then(mostrarMensagens)}, 3000);
 }
 function deuErro(erro) {
   console.log('erro ao pegar mensagens!');
@@ -57,20 +50,20 @@ function mostrarMensagens(sucesso) {
     }else if(mensagens[i].type == 'message'){
       let template = `<li data-test="message" class="message">(${mensagens[i].time})<span> ${mensagens[i].from}</span> para <span>${mensagens[i].to}</span>: ${mensagens[i].text} </li>`;
       msg.innerHTML += template;
-
     }
   }
   lastMessage = mensagens[mensagens.length - 1];
-  
-
 }
-
 function mostrarUltima() {
   lastMessage = mensagens[mensagens.length - 1];
   let msg = document.querySelector('ul');
-  // msg.innerHTML = '';  
-  let template = `<li data-test="message"><span>${lastMessage.from}</span> ${lastMessage.text} </li>`;
-  msg.innerHTML += template;
+  if(lastMessage.type == 'status') {
+    let template = `<li data-test="message" class="status">(${lastMessage.time})<span> ${lastMessage.from}</span> ${lastMessage.text} </li>`;
+    msg.innerHTML += template;
+  }else if(lastMessage.type == 'message'){
+    let template = `<li data-test="message" class="message">(${lastMessage.time})<span> ${lastMessage.from}</span> para <span>${lastMessage.to}</span>: ${lastMessage.text} </li>`;
+    msg.innerHTML += template;
+  }
 }
 
 //ENVIANDO MENSAGENS PARA O ARRAY DE MENSAGENS.
@@ -82,20 +75,8 @@ function enviarMensagem() {
     text: text,
     type: "message"
   }
-  axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem);
-
+  axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem).then(()=>{mostrarMensagens}).catch(()=>{window.location.reload()});
 }
-
-// ENVIANDO MENSAGENS PARA O SERVIDOR
-// let resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',mensagens); //objeto nova mensagem.
-// resposta.then(respostaPositiva, respostaNegativa);
-// function respostaPositiva(certo){
-//   console.log(certo);
-// }
-// function respostaNegativa(errado)  {
-//   console.log('deu errado');
-// }
-
 
 
 
